c42f3bc8a4d65baf0f5320ace757ccd5dfd126a6
Walfalcon <hyperlucas@gmail.com>
2017-05-17 09:00:46 -0700

    Start!
    So we got the starting thing now!

---
+++ Helth/HelthMain.java
@@ -0,0 +1,17 @@
+
+/**
+ * The main class
+ * All other classes will be called from here
+ * 
+ * It should only contain one method, main, and shouldn't have any of the code
+ * to actually do anything in it. Just have other classes do all the work. This
+ * is the organizer.
+ * 
+ * This will be the only class that everyone works on, so don't mess it up too much.
+ */
+public class HelthMain
+{
+    public static void main(String[] args) {
+        
+    }
+}
