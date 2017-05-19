8c119b553b60edc2badf3dea0392800a09dbc898
Walfalcon <hyperlucas@gmail.com>
2017-05-19 07:53:02 -0700

    update

--- Helth/HelthMain.java
+++ Helth/HelthMain.java
@@ -12,6 +12,6 @@
 public class HelthMain
 {
     public static void main(String[] args) {
-        
+        System.out.println(RecipeGetter.x);
     }
 }
---
+++ Helth/RecipeGetter.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class RecipeGetter here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class RecipeGetter
+{
+    // instance variables - replace the example below with your own
+    public static int x = 3;
+
+    /**
+     * Constructor for objects of class RecipeGetter
+     */
+    public RecipeGetter()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
