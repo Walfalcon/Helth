684b1cfbf31fef28e7ce717844a45febe7629225
elishachen <elisha.chen17@gmail.com>
2017-05-25 09:09:39 -0700

    Update FoodGroup.java

--- Helth/FoodGroup.java
+++ Helth/FoodGroup.java
@@ -1,33 +1,35 @@
+import java.util.ArrayList;
 
-/**
- * Write a description of class FoodGroup here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class FoodGroup
-{
-    // instance variables - replace the example below with your own
-    private int x;
+public abstract class FoodGroup {
+    
+    private ArrayList<String> grouping;
+    private double servNeeded;
+    private double additServe;
+    private ArrayList<Double> grams;
+    private double servEaten;
 
-    /**
-     * Constructor for objects of class FoodGroup
-     */
-    public FoodGroup()
-    {
-        // initialise instance variables
-        x = 0;
+    public FoodGroup(){
+        grouping = new ArrayList<String>();
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public void setGrouping(String food){
+        grouping.add(food);
+    }
+    
+    public abstract void setAmtNeeded(int age, String sex);
+    
+    public String getAmtNeeded(){
+        return servNeeded + " to " + (servNeeded+additServe) + " servings.";
+    }
+    
+    public String amtRemain(double totalServe, double servEaten){
+        totalServe = servNeeded + additServe;
+        return (totalServe - servEaten) + " servings left.";
+    }
+    
+    public abstract double calcEaten(ArrayList<Double> grams);
+    
+    public void setServEaten (double servEaten){
+        this.servEaten = servEaten;
     }
 }
