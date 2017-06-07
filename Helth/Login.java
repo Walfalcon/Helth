0a9f6e517b29c1a5466e0e115ee85fe625618677
Walfalcon <hyperlucas@gmail.com>
2017-06-07 08:06:45 -0700

    fixed login bugs

--- Helth/Login.java
+++ Helth/Login.java
@@ -4,34 +4,34 @@ public class Login
     private static String username;
     public static void signUp(){
         //Prompt the user and pick String variables
-     System.out.println("Please choose a username: ");
-     Scanner in = new Scanner(System.in);
-     String username = in.nextLine();
-     System.out.println("Please enter password:\n(Must have 6 characters and at least one non-alphanumeric character @, +, -, *, /)");
-     String password = in.nextLine();
-     System.out.println("Please confirm password: ");
-     String confirm = in.nextLine();     
-     boolean condition;
-     condition = isValid(password);
-  //Create while loop to catch if you typed confirm password in correctly   
-   while (!password.equals(confirm) || (!condition)) {
-         System.out.println("");
-         System.out.println("The password is invalid.");
-         System.out.println("Please enter a password again: ");
-         String Password = in.nextLine();
-         System.out.print("Please re-confirm the password: ");
-         String Confirm = in.nextLine();        
-         if (isValid(password)) {
-             //Print password is invalid if chosen password does not match confirmed password
-            System.out.println( "The password is valid");
-            break;
-        }
- }       
-}
-public static String getUsername() {
-    return username;
-}
-
+        System.out.println("Please choose a username: ");
+        Scanner in = new Scanner(System.in);
+        String username = in.nextLine();
+        System.out.println("Please enter password:\n(Must have 6 characters and at least one non-alphanumeric character @, +, -, *, /)");
+        String password = in.nextLine();
+        System.out.println("Please confirm password: ");
+        String confirm = in.nextLine();     
+        boolean condition;
+        condition = isValid(password);
+        //Create while loop to catch if you typed confirm password in correctly   
+        while (!password.equals(confirm) || (!condition)) {
+            System.out.println("");
+            System.out.println("The password is invalid.");
+            System.out.println("Please enter a password again: ");
+            password = in.nextLine();
+            System.out.print("Please re-confirm the password: ");
+            confirm = in.nextLine();        
+            if (isValid(password) && password.equals(confirm)) {
+                //Print password is invalid if chosen password does not match confirmed password
+                System.out.println( "The password is valid");
+                break;
+            }
+        }       
+    }
+    public static String getUsername() {
+        return username;
+    }
+    
     public static boolean isValid(String password) {
         //Checks to see if there are any alphanumeric characters in the password
         System.out.println("");
@@ -57,10 +57,8 @@ public static String getUsername() {
                     return true;
                 }
             }
-            if(!true) {
-                System.out.println("Must have at least one alphaneumeric character.");                  
-            }
+            System.out.println("Must have at least one alphaneumeric character.");
+            return false;
         }
-        return true;
     }
 }
