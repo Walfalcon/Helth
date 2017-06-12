96c88ac59f75988bf4e288d4aeefc1af2a922abb
Walfalcon <hyperlucas@gmail.com>
2017-06-12 08:53:18 -0700

    df

--- Helth/Calculator.java
+++ Helth/Calculator.java
@@ -1,15 +1,3 @@
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import java.io.*;
-
-public class Calculator extends Thread
+public class Calculator
 {
-    MediaPlayer rolling;
-    public Calculator() {
-        rolling = new MediaPlayer(new Media(new File("RecommendedDailyValues.mp3").toURI().toString()));
-    }
-    
-    public void run() {
-        rolling.play();
-    }
 }
--- Helth/HelthMain.java
+++ Helth/HelthMain.java
@@ -13,7 +13,6 @@ public class HelthMain
 {
     private static User user = new User();
     private static Diary diary;
-    private static Calculator calc = new Calculator();
     private static RecipeGetter recipeGet = new RecipeGetter(user);
     private static Login login = null;
     
@@ -64,7 +63,6 @@ public class HelthMain
                 }
                 break;
                 case CALCULATOR:
-                calc.start();
                 System.out.println("LOL joke's on you there is no calculator!");
                 break;
                 case EXIT:
