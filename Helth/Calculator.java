ed3e7287ea221831b01a11c6c08c51425639a77a
Walfalcon <hyperlucas@gmail.com>
2017-06-12 21:19:51 -0700

    bl

--- Helth/Calculator.java
+++ Helth/Calculator.java
@@ -1,9 +1,25 @@
+import java.io.File;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 /**
 *WriteadescriptionofclassCalculatorhere.
 *
 *@author(yourname)
 *@version(aversionnumberoradate)
 */
-public class Calculator
-{
+public class Calculator extends Application {
+    public static void main(String[] args) {
+        Calculator calc = new Application();
+        calc.start(new Stage());
+    }
+
+    public void start (Stage stage) {
+        StackPane pane = new StackPane();
+
+        stage.setTitle("Calculator");
+        stage.setScene(new Scene(pane, 250, 250));
+        stage.show();
+    }
 }
